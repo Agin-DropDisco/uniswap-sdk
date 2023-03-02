11408ac465d568dfce246b74337c2bfcb986f663
Agin DropDisco <76779267+Agin-DropDisco@users.noreply.github.com>
2023-03-02 16:25:20 +0700

    v2 (#1)

--- dist/constants.d.ts
+++ dist/constants.d.ts
@@ -1,7 +1,7 @@
 import JSBI from 'jsbi';
 export declare type BigintIsh = JSBI | bigint | string;
 export declare enum ChainId {
-    MAINNET = 56,
+    MAINNET = 1,
     ROPSTEN = 3,
     RINKEBY = 4,
     GÖRLI = 5,
--- dist/vam-sdk.esm.js
+++ dist/vam-sdk.esm.js
@@ -16,7 +16,7 @@ var _SOLIDITY_TYPE_MAXIMA;
 var ChainId;
 
 (function (ChainId) {
-  ChainId[ChainId["MAINNET"] = 56] = "MAINNET";
+  ChainId[ChainId["MAINNET"] = 1] = "MAINNET";
   ChainId[ChainId["ROPSTEN"] = 3] = "ROPSTEN";
   ChainId[ChainId["RINKEBY"] = 4] = "RINKEBY";
   ChainId[ChainId["G\xD6RLI"] = 5] = "G\xD6RLI";
